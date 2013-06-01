fc3d27658a01ee07f0ee636de0a5e0d7c1130eb6
Per Hallström <hallstroem@gmail.com>
2013-06-01 13:02:07 +0800

    k4t3gorizr now slurps data from the internets

--- k4t3gorizr.rb
+++ k4t3gorizr.rb
@@ -1,14 +1,25 @@
+require 'httpclient'
+require 'json'
+
 # I am using Ruby 1.9.3-p392
 # 'rvm use 1.9.3-p392'
 
+API_KEY = "9b4in3bm"
+
 def kategorise text
-  [
-      {"id" => "288", "weight" => "0.18081734", "label" => "Computers/Software/Human_Resources"},
-      {"id" => "141", "weight" => "0.17852962", "label" => "Business/Education_and_Training"},
-      {"id" => "826", "weight" => "0.17403297", "label" => "Society/Government/Multilateral"},
-      {"id" => "846", "weight" => "0.16250728", "label" => "Society/Issues/Economic"},
-      {"id" => "167", "weight" => "0.15244144", "label" => "Business/Healthcare"}
-  ]
+  c = HTTPClient.new
+  beautiful_http_reponse = c.post "http://api.semantichacker.com/#{API_KEY}/category", {
+      :content => text,
+      :showLabels => true,
+      :useShortLabels => false,
+      :nCategories => 5,
+      :format => 'json'
+  }
+  response = JSON.parse(beautiful_http_reponse.body)
+  response["categorizer"]["categorizerResponse"]["categories"]
 end
 
-kategorise "In 2011, the Independent Review of Aid Effectiveness recommended that Australia join the group as it would represent value for money, and be a high-level indication of Australia's commitment to development in Africa. The government's 2011 response to that review, An Effective Aid Program for Australia, agreed in principle to the recommendation, subject to the outcome of a detailed assessment. That detailed assessment, along with the Australian Multilateral Assessment and Comprehensive Aid Policy Framework, from 2012, and the Joint Standing Committee on Treaties report from this year have all agreed that joining the group would have positive outcomes for Australia's aid program."
+#puts kategorise "In 2011, the Independent Review of Aid Effectiveness recommended that Australia join the group as it would represent value for money, and be a high-level indication of Australia's commitment to development in Africa. The government's 2011 response to that review, An Effective Aid Program for Australia, agreed in principle to the recommendation, subject to the outcome of a detailed assessment. That detailed assessment, along with the Australian Multilateral Assessment and Comprehensive Aid Policy Framework, from 2012, and the Joint Standing Committee on Treaties report from this year have all agreed that joining the group would have positive outcomes for Australia's aid program."
+puts "Try me out! Type or paste one line of text here to see what it is about. I am always right :("
+puts ""
+puts kategorise gets
