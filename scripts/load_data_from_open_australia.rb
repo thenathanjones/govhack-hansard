fec33aab299e4e154f72ee86b909d0cc893fabd3
Nathan Jones <thenathanjones@gmail.com>
2013-06-01 21:48:34 +0800

    Simplify the script for pulling info from open australia

--- scripts/load_data_from_open_australia.rb
+++ scripts/load_data_from_open_australia.rb
@@ -1,10 +1,8 @@
-require 'timeliness'
 require 'openaustralia'
 require 'neography'
-require 'pp'
 
 key = 'CPkTYTBD4HbDBC7Y6GHDHU9H'
-
+image_root = 'http://www.openaustralia.org'
 api = OpenAustralia::Api.new key
 
 @neo = Neography::Rest.new
@@ -13,7 +11,7 @@ api = OpenAustralia::Api.new key
 @neo.create_node_index :representatives
 
 api.get_representatives.each do |rep|
-  rep_node = @neo.create_node(:person_id => rep.person_id, :member_id => rep.member_id, :full_name => rep.full_name, :party => rep.party, :image_url => rep.image_url)
+  rep_node = @neo.create_node(:person_id => rep.person_id, :member_id => rep.member_id, :full_name => rep.full_name, :party => rep.party, :image_url => image_root + rep.image_url)
   @neo.add_node_to_index :representatives, :person_id, rep.person_id, rep_node
   @neo.add_node_to_index :representatives, :member_id, rep.member_id, rep_node
   @neo.add_node_to_index :representatives, :full_name, rep.full_name, rep_node
