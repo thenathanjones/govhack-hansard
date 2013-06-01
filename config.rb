6b8703077f13cb071cdc968a1a9e933613fa8bce
Nathan Jones <thenathanjones@gmail.com>
2013-06-01 19:01:31 +0800

    Simplify the shiz.

--- config.rb
+++ config.rb
@@ -3,7 +3,8 @@
 ###
 
 # Susy grids in Compass
-require 'susy'
+# First: gem install susy
+# require 'susy'
 
 # Change Compass configuration
 # compass_config do |config|
@@ -11,22 +12,7 @@ require 'susy'
 # end
 
 ###
-# Haml
-###
-
-# CodeRay syntax highlighting in Haml
-# First: gem install haml-coderay
-# require 'haml-coderay'
-
-# CoffeeScript filters in Haml
-# First: gem install coffee-filter
-# require 'coffee-filter'
-
-# Automatic image dimensions on image_tag helper
-# activate :automatic_image_sizes
-
-###
-# Page command
+# Page options, layouts, aliases and proxies
 ###
 
 # Per-page layout changes:
@@ -51,41 +37,21 @@ require 'susy'
 # Helpers
 ###
 
+# Automatic image dimensions on image_tag helper
+# activate :automatic_image_sizes
+
 # Methods defined in the helpers block are available in templates
-helpers do
-  # Calculate the years for a copyright
-  def copyright_years(start_year)
-    end_year = Date.today.year
-    if start_year == end_year
-      start_year.to_s
-    else
-      start_year.to_s + '-' + end_year.to_s
-    end
-  end
+# helpers do
+#   def some_helper
+#     "Helping"
+#   end
+# end
 
-  # Holder.js image placeholder helper
-  def img_holder(opts = {})
-    return "Missing Image Dimension(s)" unless opts[:width] && opts[:height]
-    return "Invalid Image Dimension(s)" unless opts[:width].to_s =~ /^\d+$/ && opts[:height].to_s =~ /^\d+$/
+set :css_dir, 'stylesheets'
 
-    img  = "<img data-src=\"holder.js/#{opts[:width]}x#{opts[:height]}/auto"
-    img << "/#{opts[:bgcolor]}:#{opts[:fgcolor]}" if opts[:fgcolor] && opts[:bgcolor]
-    img << "/text:#{opts[:text].gsub(/'/,"\'")}" if opts[:text]
-    img << "\" width=\"#{opts[:width]}\" height=\"#{opts[:height]}\">"
+set :js_dir, 'javascripts'
 
-    img
-  end
-
-end
-
-# Change the CSS directory
-# set :css_dir, "alternative_css_directory"
-
-# Change the JS directory
-# set :js_dir, "alternative_js_directory"
-
-# Change the images directory
-# set :images_dir, "alternative_image_directory"
+set :images_dir, 'images'
 
 # Build-specific configuration
 configure :build do
