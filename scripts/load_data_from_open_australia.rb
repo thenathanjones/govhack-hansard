a98523b803c9f5eea47f124661c886c3eed2aa03
Nathan Jones <thenathanjones@gmail.com>
2013-06-01 21:29:49 +0800

    Load the information we're going to get from Open Australia. At this time just the representatives.

--- scripts/load_data_from_open_australia.rb
+++ scripts/load_data_from_open_australia.rb
@@ -8,20 +8,14 @@ key = 'CPkTYTBD4HbDBC7Y6GHDHU9H'
 api = OpenAustralia::Api.new key
 
 @neo = Neography::Rest.new
-@neo.set_node_auto_index_status(true)
 @neo.set_relationship_auto_index_status(true) 
 
-@neo.create_node_auto_index
-@neo.add_node_auto_index_property(:person_id)
-@neo.add_node_auto_index_property(:full_name)
-@neo.add_node_auto_index_property(:entry_date)
+@neo.create_node_index :representatives
 
-results = api.get_representatives.last(10).each do |rep|
-  rep_node = @neo.create_node(:person_id => rep.person_id, :full_name => rep.full_name, :party => rep.party, :image_url => rep.image_url)
-
-  debate_entries = api.get_debates(:person => rep.person_id, :page => 1, :type => 'representatives').results
-  debate_entries.each do |entry|
-    debate_node = @neo.create_node(:body => entry.body, :entry_date => Timeliness.parse(entry.hdate).to_time.to_i)
-    @neo.create_relationship(:said_by, debate_node, rep_node)
-  end
+api.get_representatives.each do |rep|
+  rep_node = @neo.create_node(:person_id => rep.person_id, :member_id => rep.member_id, :full_name => rep.full_name, :party => rep.party, :image_url => rep.image_url)
+  @neo.add_node_to_index :representatives, :person_id, rep.person_id, rep_node
+  @neo.add_node_to_index :representatives, :member_id, rep.member_id, rep_node
+  @neo.add_node_to_index :representatives, :full_name, rep.full_name, rep_node
+  @neo.add_node_to_index :representatives, :party, rep.party, rep_node
 end
