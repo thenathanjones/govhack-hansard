5d2a2160789245b082819b7c505887f14b94dba1
Per Hallström <hallstroem@gmail.com>
2013-06-02 10:31:22 +0800

    Cleaning up k4t3gorizr

--- scripts/k4t3gorizr.rb
+++ scripts/k4t3gorizr.rb
@@ -32,15 +32,15 @@ def konseptise text
   response = JSON.parse(beautiful_http_reponse.body)
   response["conceptExtractor"]["conceptExtractorResponse"]["concepts"]
 end
-
-#puts kategorise "In 2011, the Independent Review of Aid Effectiveness recommended that Australia join the group as it would represent value for money, and be a high-level indication of Australia's commitment to development in Africa. The government's 2011 response to that review, An Effective Aid Program for Australia, agreed in principle to the recommendation, subject to the outcome of a detailed assessment. That detailed assessment, along with the Australian Multilateral Assessment and Comprehensive Aid Policy Framework, from 2012, and the Joint Standing Committee on Treaties report from this year have all agreed that joining the group would have positive outcomes for Australia's aid program."
-puts "Try me out! Type or paste one line of text here to see what it is about. I am always right :("
-puts ""
-what_you_type = gets
-
-puts "────────────────────"
-puts "CATEGORIES"
-puts kategorise what_you_type
-puts ""
-puts "CONCEPTS"
-puts konseptise what_you_type
+#
+##puts kategorise "In 2011, the Independent Review of Aid Effectiveness recommended that Australia join the group as it would represent value for money, and be a high-level indication of Australia's commitment to development in Africa. The government's 2011 response to that review, An Effective Aid Program for Australia, agreed in principle to the recommendation, subject to the outcome of a detailed assessment. That detailed assessment, along with the Australian Multilateral Assessment and Comprehensive Aid Policy Framework, from 2012, and the Joint Standing Committee on Treaties report from this year have all agreed that joining the group would have positive outcomes for Australia's aid program."
+#puts "Try me out! Type or paste one line of text here to see what it is about. I am always right :("
+#puts ""
+#what_you_type = gets
+#
+#puts "────────────────────"
+#puts "CATEGORIES"
+#puts kategorise what_you_type
+#puts ""
+#puts "CONCEPTS"
+#puts konseptise what_you_type
